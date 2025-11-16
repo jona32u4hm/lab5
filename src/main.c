675cc1bf47abb287c4b6cdfebfe9c07540739956
sebas-araya <arayafukssebastian@gmail.com>
2025-11-16 15:45:24 -0600

    arhcivo main.c para el stack

--- src/main.c
+++ src/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include "stack.h"
 #include "../include/doubly_linked_list.h"
 
 DoublyList myList;
 
-int main(){
+int main_doublylist(){
 	printf("testing doubly_linked_list.h\n");
 	printf("\ninsert head 5 (empty list)\n");
 	doublyInsertHead(&myList, 5);
@@ -59,4 +60,51 @@ int main(){
     printf("list size now: %d\n", myList.size);
 	
 	return 0;
+}
+
+int main_stack(){
+    struct Stack s;
+    initStack(&s); //incia el stack
+
+    //aquí va a ir el push de elementos:
+    push(&s, 10);
+    printf("Push: 10 \n");
+    push(&s, 20);
+    printf("Push: 20 \n");
+    push(&s, 30);
+    printf("Push: 30 \n");
+    int val;
+
+    //pop
+    pop(&s, &val);
+    printf("Pop: %d\n", val);
+	//top
+    top(&s, &val);
+    printf("Top: %d\n", val);
+
+    //Verificar si está vacío
+    if (isEmpty(&s)) {
+        printf("El stack está vacío\n");
+    }
+    else {
+        printf("El stack no está vacío\n");
+    }
+
+    printf("A continuación, se va a vaciar el stack... \n");
+
+    //Pop hasta vaciar stack para que esté vacío
+    pop(&s, &val);
+    pop(&s, &val);
+    if (isEmpty(&s)) {
+        printf("El stack está vacío\n");
+    }
+    else {
+        printf("El stack no está vacío\n"); //debe imprimir que está vacío
+    } 
+}
+
+int main(){
+    main_doublylist();
+    main_stack();
+    return 0;
 }
