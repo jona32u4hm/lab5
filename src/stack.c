2b3d355299f4d0b96dd15b1504789529293aab09
sebas-araya <arayafukssebastian@gmail.com>
2025-11-16 12:40:23 -0600

    primer avance en stack.c

--- src/stack.c
+++ src/stack.c
@@ -1,3 +1,24 @@
 #include "stack.h"
 
-void initStack(struct Stack* s)
+void initStack(struct Stack* s) {
+    s->top=-1; //indica stack vacío
+}
+
+int isEmpty(struct Stack* s) {
+    return (s->top==-1); //verifica si el stack está vacío
+}
+
+int push(struct Stack*s, int value) { //agrega un elemento al inicio del stack
+    if (s->top >== MAX_STACK -1) {
+        return 0; //verifica si el stack está lleno
+    }
+    s-> top++;
+    s->data[s->top]=value;
+    return 1; //si no está lleno, devuelve 1
+}
+
+int pop(struct Stack* s, int* value) { //elimina y delvuelve el elemento superior
+    if (IsEmoty(s)) { 
+        return 0; //falla si el stack está vacío
+    }
+}
