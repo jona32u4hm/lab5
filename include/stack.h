91ab786bcfffa4c038f8d5e38e07e75a1d03b626
sebas-araya <arayafukssebastian@gmail.com>
2025-11-15 19:51:17 -0600

    stack.h terminado

--- include/stack.h
+++ include/stack.h
@@ -1,4 +1,22 @@
 #ifndef STACK_H
 #define STACK_H
 
-#include <stdbool.h>
+struch StackNode {
+    int data;
+    struct StackNode* next;
+}
+//esta parte es la estructura de un nodo del stack
+
+struct Stack {
+    struct StackNode* top;
+}
+//estructura del stack
+
+void initStack(struct Stack* stack); //crea stack vacío
+void push(struct Stack* stack, init value); //elemento a parte superior
+int pop(struct Stack* stack); //elimina y retorna elemento superior
+int pek(struct Stack* stack); //devuelve elemento superior sin eliminarlo
+int isEmpty(struct Stack stack);//verifica stack vacío
+void freeStack(struct Stack* stack); //libera nodos
+
+#endif
